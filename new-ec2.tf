8b26a99414d29d61d70a300ee4ef913561881f51
Johnwick4488 <115544081+Johnwick4488@users.noreply.github.com>
2024-01-31 12:00:42 +0530

    Update new-ec2.tf

--- new-ec2.tf
+++ new-ec2.tf
@@ -1,12 +1,11 @@
 module "ec2_instance" {
-  source  = "./module/ec2/"
-  region = "us-east-1"
+  source        = "./module/ec2/"
+  region        = "us-east-1"
 
-  name = "New-Instance"
-  instance_type          = "t2.micro"
+  instance_type = "t2.micro"
   ami           = "ami-0a3c3a20c09d6f377"
-  key_name               = "My-new-key"
-  tag = {
+  key_name      = "My-new-key"
+  tag           = {
     Name   = "Santosh"
     Environment = "Prod"
   }
