11a492a0e3cd243465c0001d9c124e0eb1e5eb54
Johnwick4488 <115544081+Johnwick4488@users.noreply.github.com>
2024-01-31 11:46:18 +0530

    Update new-ec2.tf

--- new-ec2.tf
+++ new-ec2.tf
@@ -6,7 +6,7 @@ module "ec2_instance" {
   instance_type          = "t2.micro"
   ami           = "ami-0a3c3a20c09d6f377"
   key_name               = "My-new-key"
-  tags = {
+  tag = {
     Name   = "Santosh"
     Environment = "Prod"
   }
