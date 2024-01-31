c4a3489f25c4ade6aca0ed4065d777a0e946f898
Johnwick4488 <115544081+Johnwick4488@users.noreply.github.com>
2024-01-31 13:16:31 +0530

    Create new-s3.tf

---
+++ new-s3.tf
@@ -0,0 +1,13 @@
+module "s3_bucket" {
+  source = "./module/s3/"
+
+  bucket = "santosh-445588"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
