1c07b97e803ab80559c87c7dc276c45affe925d9
Johnwick4488 <115544081+Johnwick4488@users.noreply.github.com>
2024-01-31 11:49:47 +0530

    Update main.tf

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -2,8 +2,6 @@ resource "aws_instance" "this" {
   name          = var.name
   instance_type = var.instance_type
   ami           = var.ami
-  subnet_id     = var.subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids
   key_name      = var.key_name
   tags          = var.tag
 }
