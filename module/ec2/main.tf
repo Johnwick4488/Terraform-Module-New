044fe162fa204f9b350aa4fa0fc77479bf62401a
Johnwick4488 <115544081+Johnwick4488@users.noreply.github.com>
2024-01-31 11:40:40 +0530

    Update main.tf

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -1,5 +1,23 @@
 resource "aws_instance" "this" {
-  ami           = "ami-0a3c3a20c09d6f377"
+  name          = var.name
+  instance_type = var.instance_type
+  ami           = var.ami
+  key_name      = var.key_name
+  tags          = var.tag
+}
+
+
+
+
+
+
+
+
+
+
+
+
+ ami           = "ami-0a3c3a20c09d6f377"
   instance_type = "t2.micro"
 
   tags = {
