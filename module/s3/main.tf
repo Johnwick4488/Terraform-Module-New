fbb11a7b685fa12d7ef19c51566df15fdc9c07b8
Johnwick4488 <115544081+Johnwick4488@users.noreply.github.com>
2024-01-31 13:31:11 +0530

    Update main.tf

--- module/s3/main.tf
+++ module/s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "this" {
   bucket                      = var.bucket
-  acl                         = var.acl
+  aws_s3_bucket_acl           = var.acl
   tags                        = var.tags
 }
