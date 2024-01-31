3bfce7dc9261ffdcfd181835585830f2cdfd9bc8
root <root@ip-172-31-41-141.ec2.internal>
2024-01-31 05:52:27 +0000

    edited

--- new-ec2.tf
+++ new-ec2.tf
@@ -2,7 +2,7 @@ module "ec2_instance" {
   source  = "./module/ec2/"
   region = "us-east-1"
 
-  name = "My-instance"
+
   instance_type          = "t2.micro"
   ami           = "ami-0a3c3a20c09d6f377"
 }
