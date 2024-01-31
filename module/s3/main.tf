9b82463a5a73c1a3a0d455a06dd5a37f910f509d
Johnwick4488 <115544081+Johnwick4488@users.noreply.github.com>
2024-01-31 12:59:30 +0530

    Create main.tf

---
+++ module/s3/main.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "this" {
+  count = local.create_bucket ? 1 : 0
+
+  bucket        = var.bucket
+  bucket_prefix = var.bucket_prefix
+
+  force_destroy       = var.force_destroy
+  object_lock_enabled = var.object_lock_enabled
+  tags                = var.tags
+}
