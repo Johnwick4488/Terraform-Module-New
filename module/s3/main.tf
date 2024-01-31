a26c8ad24db22f938bb46184a237452548c425d8
Johnwick4488 <115544081+Johnwick4488@users.noreply.github.com>
2024-01-31 13:22:37 +0530

    Update main.tf

--- module/s3/main.tf
+++ module/s3/main.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "this" {
   bucket                      = var.bucket
+  create_bucket               = var.create_bucket
   acl                         = var.acl
   control_object_ownership    = var.control_object_ownership
   object_ownership            = var.object_ownership
